c451576d120f5722a29f91d34a09be3cba5a8f8a
Fabio Conceicao <fabio.conceicao@unilever.com>
2021-05-21 10:36:21 -0300

    add folder windows

--- windows/open_document_plugin.cpp
+++ windows/open_document_plugin.cpp
@@ -14,17 +14,6 @@
 #include <memory>
 #include <sstream>
 
-#include <shlobj.h>
-#include <string.h>
-#include <Shlwapi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "include/open_document/filesystem.hpp" 
-#include <iostream>               // for std::cout
-
-#pragma comment(lib, "shell32.lib")
-
-
 namespace {
 
 class OpenDocumentPlugin : public flutter::Plugin {
@@ -67,36 +56,21 @@ OpenDocumentPlugin::~OpenDocumentPlugin() {}
 void OpenDocumentPlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-
-    if (method_call.method_name().compare("getPathDocument") == 0) {
-        std::ostringstream version_stream;
-        std::filesystem::current_path();
-        version_stream << filesytem;
-        result->Success(flutter::EncodableValue(version_stream.str()));
+  if (method_call.method_name().compare("getPlatformVersion") == 0) {
+    std::ostringstream version_stream;
+    version_stream << "Windows ";
+    if (IsWindows10OrGreater()) {
+      version_stream << "10+";
+    } else if (IsWindows8OrGreater()) {
+      version_stream << "8";
+    } else if (IsWindows7OrGreater()) {
+      version_stream << "7";
     }
-    else {
-        result->NotImplemented();
-    }
+    result->Success(flutter::EncodableValue(version_stream.str()));
+  } else {
+    result->NotImplemented();
+  }
 }
-
-
- std::string GetWorkingDirectory()
-{
-     try {
-         char my_documents[MAX_PATH];
-         HRESULT h_result = SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, my_documents);
-
-         if (h_result != S_OK) {
-            return std::string(my_documents));
-         }
-         else {
-             return std::string(my_documents));
-         }
-     }cath(e) {
-         return "ERORORORORO";
-     }
-}
-
 
 }  // namespace
 
